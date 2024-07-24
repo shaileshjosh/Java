5f217ed7dee4f35807609e5f7cf8043832f98c69
Shailesh Parkhi <shailesh.parkhi@joshsoftware.com>
2024-07-24 15:56:11 +0530

    Assignment5
    Create a spring based application with Class Student and Address. Then create beans of Student and Address class and inject Address bean as a dependency in Student class. Student class should have getter and setter methods to get and set the first name, last name, roll no, department and Address.

---
+++ eclipse-workspace/springexample/src/main/java/com/springexample/springexample/Address.java
@@ -0,0 +1,23 @@
+package com.springexample.springexample;
+
+public class Address {
+	String city;
+
+	public Address(String city) {
+		super();
+		this.city = city;
+	}
+
+	public Address() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+}
